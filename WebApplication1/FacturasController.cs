7adbf7b29aea04bfe478d466b366c781abda0f60
Francisco Sierra Luciarte <francisco.sierra.luciarte@hotmail.com>
2020-08-25 13:52:10 +0200

    25-08-2020

---
+++ WebApplication1/FacturasController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class FacturasController : ApiController
+    {
+
+        static List<Factura> Facturas = new List<Factura>();
+
+        static FacturasController()
+        {
+            Facturas.Add(new Factura(1, "Ordenador"));
+            Facturas.Add(new Factura(2, "Tablet"));
+        }
+        
+        // GET api/<controller>
+        public IList<Factura> Get()
+        {
+            return Facturas;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] Factura f)
+        {
+            Facturas.Add(f);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/Models/Factura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Factura
+    {
+        public int Numero { get; set; }
+        public string Concepto { get; set; }
+
+        public Factura(int numero, string concepto)
+        {
+            Numero = numero;
+            Concepto = concepto;
+        }
+
+        public Factura()
+        {
+
+        }
+    }
+}
